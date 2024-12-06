c0eb95b9637712d570095fba0c959fca1db100a4
SornDev <sorndev.teach@gmail.com>
2024-12-06 21:11:57 +0700

    install template VDO 03

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Minipos 21</title>
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+     <!-- Core CSS -->
+     <link rel="stylesheet" href="{{ url('assets/vendor/css/core.css') }}" class="template-customizer-core-css">
+    <link rel="stylesheet" href="{{ url('assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css">
+    <link rel="stylesheet" href="{{ url('assets/css/demo.css') }}">
+
     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1> Hello Laravel 123456789</h1>
-    <hr>
+
     <div id="app-vue" ></div>
 
     @vite('resources/js/app.js')
+
+  <script src="{{ url('assets/vendor/libs/jquery/jquery.js') }}"></script>
+  <script src="{{ url('assets/vendor/libs/popper/popper.js') }}"></script>
+  <script src="{{ url('assets/vendor/js/bootstrap.js') }}"></script>
+  <script src="{{ url('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+  <script src="{{ url('assets/vendor/js/menu.js') }}"></script>
+
 </body>
 </html>
